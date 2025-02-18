530a1d96ff4c023c1830f2dd5dc3c7a4caf27aa7
Apaetus <katharina00000@gmail.com>
2025-02-18 11:47:50 +0300

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import { text } from './app';
+import './main.scss';
+
+function component(text: string) {
+    const element = document.createElement('h1');
+    element.textContent = text;
+    return element;
+}
+
+document.body.prepend(component('Проект собран на Webpack'));
+
+console.log(text);
---
+++ webpack.config.js
@@ -0,0 +1,86 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+const FileManagerPlugin = require('filemanager-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const webpack = require('webpack');
+
+module.exports = {
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'index.[contenthash].js',
+        assetModuleFilename: path.join(
+            'images',
+            '[name].[contenthash][ext]'
+        ),
+        clean: true,
+    },
+    entry: path.join(__dirname, 'src', 'index.ts'),
+    devtool: 'inline-source-map',
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.(scss|css)$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'postcss-loader',
+                    'sass-loader',
+                ],
+            },
+            {
+                test: /\.(png|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+            // {
+            //     test: /\.svg$/,
+            //     type: 'asset/resource',
+            //     generator: {
+            //         filename: path.join(
+            //             'icons',
+            //             '[name].[contenthash][ext]'
+            //         ),
+            //     },
+            // },
+        ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, 'src', 'template.html'),
+            filename: 'index.html',
+        }),
+        new FileManagerPlugin({
+            events: {
+                // onStart: {
+                //     delete: ['dist'],
+                // },
+                onEnd: {
+                    copy: [
+                        {
+                            source: path.join('src', 'static'),
+                            destination: 'dist',
+                        },
+                    ],
+                },
+            },
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].[contenthash].css',
+        }),
+        new webpack.HotModuleReplacementPlugin(),
+    ],
+
+    devServer: {
+        watchFiles: path.join(__dirname, 'src'),
+        open: true,
+        hot: true,
+        port: 9000,
+    },
+};
